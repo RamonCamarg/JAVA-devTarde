bc4221a606931122c4cfea0f359c133e86b81d78
RamonCamarg <camargoramon15@gmail.com>
2023-03-07 16:44:27 -0300

    07/03/23

---
+++ ExercicioIF1/App.java
@@ -0,0 +1,16 @@
+package ExercicioIF1;
+
+public class App {
+    public static void main(String[] args) {
+        Exercicio1 obj1 = new Exercicio1();
+        obj1.ex1();
+        obj1.ex2();
+        obj1.ex3();
+        obj1.ex4();
+        obj1.ex5();
+        obj1.ex6();
+        obj1.ex7();
+    }
+    
+    
+}
