ffab4003d201b6a3b9bbdfb612ef09de3d421487
RamonCamarg <camargoramon15@gmail.com>
2023-08-03 16:50:31 -0300

    03/08/23

---
+++ RevisaoFPOO/Cadastro/Cachorro.java
@@ -0,0 +1,16 @@
+package Cadastro;
+
+public class Cachorro extends Animais{
+    boolean pulgas;
+
+
+
+    public boolean isPulgas() {
+        return pulgas;
+    }
+
+    public void setPulgas(boolean pulgas) {
+        this.pulgas = pulgas;
+    }
+    
+}
---
+++ RevisaoFPOO/Cadastro/Gato.java
@@ -0,0 +1,15 @@
+package Cadastro;
+
+public class Gato extends Animais{
+    boolean pulgas;
+
+
+
+    public boolean isPulgas() {
+        return pulgas;
+    }
+
+    public void setPulgas(boolean pulgas) {
+        this.pulgas = pulgas;
+    }
+}
